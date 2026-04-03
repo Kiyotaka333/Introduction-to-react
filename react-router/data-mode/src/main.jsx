fa77b99f7f02bb8d69fa509376d1bf30a775149f
rajkumar <kiyotakaayanokoji7785@gmail.com>
2026-04-03 08:34:39 +0545

    helloo

--- react-router/data-mode/src/main.jsx
+++ react-router/data-mode/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')).render(
+import RootLayout from "./components/RootLayout";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement:<NotFound/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
--- react-router/data-mode/src/pages/HomePage.jsx
+++ react-router/data-mode/src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router";
 export default function HomePage(){
     return(
         <main>
             <h1>Home Page</h1>
+            <Link to="/contact">
+            <button>Go to \Contact Page</button>
+            </Link>
         </main>
     );
 }
